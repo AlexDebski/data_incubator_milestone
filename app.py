af022d1b3b9f9aec225e720f27017158168c0c28
Alex <alexdebski@gmail.com>
2022-01-09 17:14:04 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,51 @@
+import requests as rs
+import pandas as pd
+import json
+import streamlit as st
+import plotly.express as px
+
+
+st.sidebar.subheader('Select ticker symbol')
+ticker = st.sidebar.text_input('Ticker symbol e.g TSLA')
+
+if ticker != '':
+
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey='
+
+
+
+    r = rs.get(url)
+    data = r.json()
+
+    def createDF(x):
+        advdict = {}
+        #advdict['Symbol'] = {}
+        advdict['Date'] = {}
+        advdict['Closing'] = {}
+        #symbol = x['Meta Data']['2. Symbol'] 
+        date = list(d['Time Series (Daily)'].keys()) 
+        closing = []
+        for y in d['Time Series (Daily)'].keys(): 
+            closing.append(d['Time Series (Daily)'][y]['4. close'])
+        for y in range(len(closing)):
+            #advdict['Symbol'].update({y:symbol})
+            advdict['Date'].update({y:date[y]})
+            advdict['Closing'].update({y:closing[y]})
+        return pd.DataFrame(advdict)
+
+    d = data
+    df = createDF(d)
+
+    df['Closing'] = pd.to_numeric(df['Closing'])
+
+    start_date, end_date = '2021-01-01', '2021-01-31'
+    df = df.query('Date >= @start_date and Date <= @end_date')
+
+
+    st.subheader(f'Unadjusted closing price for {ticker} Jan 2021')
+
+    fig = px.line(df, x='Date', y='Closing')
+    st.write(fig)
+
+else:
+    st.subheader(f'Unadjusted closing price for --- Jan 2021')
